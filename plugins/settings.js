3a656cd052acf00e3013697d2836c95e99072d07
DARK-SHANU <153761002+DARK-SHANU@users.noreply.github.com>
2024-09-01 13:07:40 +0530

    Update settings.js

--- plugins/settings.js
+++ plugins/settings.js
@@ -10,58 +10,8 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         return await conn.sendMessage(from, {
-            image: { url: 'https://telegra.ph/file/c58b6c133ce2a0233d9d2.jpg' },
-            caption: `
-                
-╭──「⚙Black Amda Settings List」───●●►
-│               
-│  ┌──────────────        
-│  ├─\n\n*Alive IMG*\n.update ALIVE_IMG: Your Image Url
-│  ├─\n\n*Alive MSG*\n.update ALIVE_MSG: Your Alive MSG 
-│  └──────────────
-│
-│  ┌──────────────        
-│  ├─\n\n*Menu IMG*\n.update Menu_IMG: Your Image Url  
-│  ├─\n\n*Menu MSG*\n.update Menu_MSG: Your Menu MSG
-│  └──────────────
-│
-│  ┌──────────────
-│  ├─ ►\n\n*AUTO REED STATUS*\n.update AUTO_READ_STATUS:
-│  ├─ 🔛 Enable 
-│  ├─ 📴 Disable 
-│  └──────────────
-│
-│  ┌──────────────
-│  ├─ ►\n\n*WORK_TYPE*\n.update WORK_TYPE: 
-│  ├─ 👤 private
-│  ├─ 👥 public 
-│  ├─ 📥👥 index or group
-│  └──────────────
-│
-│  ┌──────────────
-│  ├─ ►\n\n*Prefix*\n.update PERFIX: your prefix (
-│  | Set . as the prefix
-│  | Set , as the prefix
-│  | Set ! as the prefix
-│  | Set @ as the prefix
-│  | Set # as the prefix
-│  | Set $ as the prefix
-│  | Set % as the prefix
-│  | Set ^ as the prefix
-│  | Set & as the prefix
-│  | Set * as the prefix
-│  | Set + as the prefix
-│  | Set = as the prefix
-│  | Set / as the prefix
-│  | Set - as the prefix
-│  | Set : as the prefix
-│  | Set ~ as the prefix )
-│  
-│   ʙʟᴀᴄᴋ ᴀᴍᴅᴀ • ᴍᴜʟᴛɪ ᴅᴇᴠɪᴄᴇ
-╰───────────────────────────────────●●►
-`
-         
-        
+            image: { url: 'https://telegra.ph/file/959a00b8f92106a8856de.jpg' },
+            caption: '𝗗𝗜𝗟𝗔𝗠𝗗 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦 ⚙️\n\n*Cheng Alive&Menu IMG*\n.update ALIVE_IMG: Your Image Url\n\n*Cheng Alive MSG*\n.update ALIVE_MSG: Your Alive MSG\n\n*Cheng Prefix*\n.update PERFIX: your prefix (.,$#%&)\n\n*Auto Status Seen*\n.update AUTO_READ_STATUS: true or false\n\n*Mode*\n.update MODE: private, public, index or group'
         }, { quoted: mek });
     } catch (e) {
         console.log(e);
